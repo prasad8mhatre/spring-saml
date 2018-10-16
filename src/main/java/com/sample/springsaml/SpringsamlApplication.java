2970e404804b63f3a2c7140207619b73a2f6da93
prasad mhatre <prasad8mhatre@gmail.com>
2018-10-16 22:38:17 +0530

    init commit

---
+++ src/main/java/com/sample/springsaml/SpringsamlApplication.java
@@ -0,0 +1,12 @@
+package com.sample.springsaml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsamlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsamlApplication.class, args);
+	}
+}
